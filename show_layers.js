acff9293117b9852a25f3d34c4458df7e6b5c4e5
unknown <hhakaos@gmail.com>
2013-11-19 18:51:00 +0100

    Small changes in styles and refatoring

--- show_layers.js
+++ show_layers.js
@@ -1,32 +1,4 @@
-
-
-function addStyle(elem){
-  var red = Math.floor(Math.random() * 255)
-  var green = Math.floor(Math.random() * 255)
-  var blue = Math.floor(Math.random() * 255)
-
-  $(elem).css("border","4px solid rgb("+red+","+green+","+blue+")");
-  $(elem).css("margin", "2px");
-  $(elem).attr("title",elem);
-  console.log("Estilo aplicado a: "+ elem)
-  $(elem).attr("data-elem-name", elem)
-}
-
-function clearStyles(){
-  //Remove styles
-  $("style,link").each(function(i,elem){$(this).remove();})
-
-  //Load stules
-  //document.createStyleSheet('style.css');
-  $("head").append($("<link rel='stylesheet' href='css/template/show_layers.css' type='text/css' media='screen' />"));
-}
-
-
-
-
-function showLayers(){
-
-  var elements = Array(
+var elements = Array(
     //Common
     "#loginDialog",
     "#model_msg",
@@ -45,19 +17,107 @@ function showLayers(){
     "#page-rsss-icons",
     
     "#main-container",
-      "#main-container",
+      "#main-wrapper",
+        ".alert",
+        "#content",
+          "#eshop-catalog",
+            ".catalog-category-filter",
+            ".catalog-name-filter",
+
+
 
     //Product Detail
     ".price-2x1",
-    "#content"
-
+    ".price-actual",
+    ".product-description",
+    ".product-categories",
+    ".product-attributes-selector",
+    ".product-attributes-selector-data",
+    ".product-size",
+    ".product-prices",
+    ".product-color",
+    ".product-amount",
+    ".product-name",
+    ".preview-carousel-slider",
+    ".page-title"
   );
+
+
+function addStyle(elem){
+  var red = Math.floor(Math.random() * 200)
+  var green = Math.floor(Math.random() * 200)
+  var blue = Math.floor(Math.random() * 200)  
+
+  var $elem = $(elem);
+
+  $elem.addClass("spy");
+
+  $elem.attr("title",elem);
   
+  var $position = $elem.css("position");
+  if($position == "relative" || $position == "absolute"){
+    $elem.prepend('<div class="layer-name">'+ $elem.attr("data-parent") +'-> ' + $elem.prop('tagName') + elem + ' (' + $position + ')</div>')
+  }else{
+    $elem.prepend('<div class="layer-name">' + $elem.prop('tagName') + elem + '</div>')
+  }
+  $elem.css("border","4px solid rgb("+red+","+green+","+blue+")");
+  $(elem + " .layer-name").css("background-color","rgb("+red+","+green+","+blue+")");
+}
+
+function clearStyles(){
+  //Remove styles
+  
+  $(elements).each(function(i,elem){
+    $elem = $(elem);
+    
+    var position = $elem.css("position");
+    if(position == "relative" || position == "absolute"){
+     
+     $parents = $elem.parents();
+
+     var i = 0;
+     while($parents.eq(i).length > 0 && ($parents.eq(i).css("position") != "absolute" && $parents.eq(i).css("position") != "relative")){ i++; }
+
+     if( $parents.eq(i).attr("id") )
+      $elemParent = $parents.eq(i).attr("id") + " ("+$parents.eq(i).css("position")+")"; 
+     else if( $parents.eq(i).attr("class") )
+      $elemParent = $parents.eq(i).attr("class") + " ("+$parents.eq(i).css("position")+")";
+     else{
+      $position = $parents.eq(i).css("position");
+      if(!$position)
+        $position="static";
+      $elemParent = "body ("+$position+")"; 
+     }
+
+     $elem.attr("data-parent", $elemParent);
+
+    }
+
+  });
+  
+  //changeStyles()
+  setTimeout("changeStyles()",1000);
+  
+}
+
+function changeStyles(){
+  console.log("Lanzado!")
+  $("style,link").each(function(i,elem){$(this).remove();})
+
+  //Load stules
+  //document.createStyleSheet('style.css');
+  $("head").append($("<link rel='stylesheet' href='http://localhost:8081/show_layers.css' type='text/css' media='screen' />"));
+}
+
+
+function showLayers(){
+
   for(var i = 0; i < elements.length; i++)
     addStyle(elements[i]);
   
 
 }
 
+clearStyles();
 showLayers();
 
